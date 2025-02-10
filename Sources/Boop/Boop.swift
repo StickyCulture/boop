b9806a71b5a59b002d0519ee2dd2c686a03924ea
Justin Seibert <justinseibert@users.noreply.github.com>
2025-02-10 14:05:38 -0800

    style: re-indents lines

--- Sources/Boop/Boop.swift
+++ Sources/Boop/Boop.swift
@@ -21,7 +21,7 @@ public class Boop {
     public var isDisabled: Bool
     /// If `true`, disables the booping of sessions (including those that might be automatically booped based on other event activity)
     public var isSessionTrackingDisabled: Bool
-
+    
     /// Set this value to automatically subtract the duration of an inactivity timeout when booping sessions. Defaults to `0`.
     public var sessionTimeout: TimeInterval
     /// This value is internally flipped during the very first user-initiated event in order to ensure the first session (after app launch) includes a starting boop.
@@ -54,7 +54,7 @@ public class Boop {
                 log.notice("Firebase has been configured with default settings")
             }
         }
-
+        
         self.db = Firestore.firestore()
         self.application = application
         self.isDevelopment = isDevelopment
@@ -69,7 +69,7 @@ public class Boop {
         if self.isDisabled {
             return nil
         }
-
+        
         if !self.isSessionTrackingDisabled,
            !self.didSessionStart,
            isUserInitiated,
@@ -100,7 +100,7 @@ public class Boop {
         }
         return ref
     }
-
+    
     public func trackAppLaunch(label: String? = nil, value: String? = nil) -> DocumentReference? {
         self.sessionStart = Date()
         return self.trackEvent(event: "App Launch", label: label, value: value, isUserInitiated: false)
@@ -130,12 +130,12 @@ public class Boop {
         if !self.didSessionStart {
             return nil
         }
-
+        
         var label = "Session Duration in Milliseconds"
         if self.sessionTimeout > 0 {
             label += " (minus Timeout delay)"
         }
-
+        
         var value = 0
         if let sessionStart = self.sessionStart {
             value = Int((Date().timeIntervalSince(sessionStart) - self.sessionTimeout) * 1000)
